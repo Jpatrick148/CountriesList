c75761d9887e609c04938a6aef830721f4f057c3
Joseph <Jpatrick148@Gmail.com>
2018-11-12 15:06:14 -0500

    Done

--- CountryColorsStarterKit/country.js
+++ CountryColorsStarterKit/country.js
@@ -7,12 +7,46 @@ class Country{
     }
     setColors(){
         
+    
     }
+    
 }
 
 
 
+var Merica = {name: "Merica",lang:"Merican",greeting:"Hello WOrld",colors:["Red","White","Blue"]}
+var Canadia = {name:"Canadia",lang:"Le French",greeting:"Bonjour le monde",colors:["Red","White"]}
+var Germany = {name: "Germany", lang:"German",greeting:"Hallo Welt",colors:["Black","Red","Gold"]}
+var Finland = {name:"Finland",lang:"Finnish",greeting:"Perkele",colors:["White","Blue"]}
+var Norway = {name:"Norway",lang:"Norwegian",greeting:"Helvete",colors:["Red","Blue","White"]}
+
+
+
 function SwitchCountry(){
-    let input = prompt("Please input a country");
+    let input = $("#CountryDropdown option:selected").val(); 
+    var country;
     
+    if(input === "Merica"){
+         country = Merica;
+    }
+    else if (input === "Canadia"){
+         country = Canadia;
+    }
+    else if(input === "Germany"){
+         country = Germany
+    }
+    else if (input === "Finland"){
+         country = Finland
+    }
+    else if(input === "Norway"){
+     country = Norway
+    }
+
+    $("#Color1").css("background-color",country.colors[0])
+    $("#Color2").css("background-color",country.colors[1])
+    $("#Color3").css("background-color",country.colors[2])
+
+    $("#CountryName").text(country.name);
+    $("#OfficialLanguage").text(country.lang);
+    $("#HelloWorld").text(country.greeting);
 }
